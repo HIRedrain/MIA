faf4b67c0e7b63f9774db87c1b4a2eb499229a26
HIRedrain <redrain@yu.ac.kr>
2026-08-10 16:43:44 +0900

    :memo: docs - 주석 처리

--- src/main/kotlin/mia/service/PostService.kt
+++ src/main/kotlin/mia/service/PostService.kt
@@ -1,3 +1,20 @@
+/* PostService.kt
+* MIA - 인스타그램 자동화
+* 인스타그램 게시물 관련 이벤트를 실제 데이터로 변환 후 비즈니스 규칙대로 처리
+* 작성자 : 이홍비
+* 최초 작성 날짜 : 2026.08.09
+*
+* ========================================================
+* 프로그램 수정 / 보완 이력
+* ========================================================
+* 작업자        날짜        수정 / 보완 내용
+* ========================================================
+* 이홍비    2026.08.09     Service 생성
+* 이홍비    2026.08.10     post crud 처리
+* ========================================================
+*/
+
+
 package mia.service
 
 import mia.dto.InstagramMediaResponse
@@ -5,14 +22,13 @@ import mia.dto.PostCreateRequest
 import mia.entity.Post
 import mia.repository.PostRepository
 import org.springframework.stereotype.Service
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Service
 class PostService (
     private val postRepository: PostRepository) {
 
-    fun savePost(request: PostCreateRequest) {
+    fun createPost(request: PostCreateRequest) {
 
         val media = getMediaFromPostURL(request.postURL)
         if (media == null) {
@@ -38,7 +54,7 @@ class PostService (
 
 
 
-    fun savePostTest(request: PostCreateRequest) {
+    fun createPostTest(request: PostCreateRequest) {
 
         println("✅ $request")
 
