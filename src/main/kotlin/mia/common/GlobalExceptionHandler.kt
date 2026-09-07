28aabfb4fc6006abbb79faf60cc5736284442097
HIRedrain <redrain@yu.ac.kr>
2026-09-07 13:02:47 +0900

    :sparkles: feat - GlobalExceptionHandler : 전역 예외 처리 클래스

---
+++ src/main/kotlin/mia/common/GlobalExceptionHandler.kt
@@ -0,0 +1,39 @@
+/* GlobalExceptionHandler.kt
+* MIA - 인스타그램 자동화
+* 전역 예외 처리 클래스
+* 작성자 : 이홍비
+* 최초 작성 날짜 : 2026.09.07
+*
+* ========================================================
+* 프로그램 수정 / 보완 이력
+* ========================================================
+* 작업자        날짜        수정 / 보완 내용
+* ========================================================
+* 이홍비    2026.09.07     GlobalExceptionHandler 생성
+* ========================================================
+*/
+
+package mia.common
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(
+        e: IllegalArgumentException
+    ): ResponseEntity<Map<String, String?>> {
+
+        return ResponseEntity
+            .badRequest()
+            .body(
+                mapOf(
+                    "message" to e.message
+                )
+            )
+    }
+}
