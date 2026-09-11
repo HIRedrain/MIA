04cb33f499ed1f4d60aeb6d41e1748f66ee6339b
HIRedrain <redrain@yu.ac.kr>
2026-09-11 10:30:48 +0900

    :hammer: fix - 의존성 주입

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,14 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 kotlin {
